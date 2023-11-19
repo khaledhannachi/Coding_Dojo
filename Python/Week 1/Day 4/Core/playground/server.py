5e77aa7b6f3307c30be36f151f665a948d8c5fe3
khaledhannachi <hannachikhaled22@gmail.com>
2023-11-19 11:14:05 +0100

    playground

---
+++ Python/Week 1/Day 4/Core/playground/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/play/<int:number>")
+@app.route("/play/<int:number>/<color>")
+def play(number, color):
+    return render_template("index.html", number=number, color=color)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
