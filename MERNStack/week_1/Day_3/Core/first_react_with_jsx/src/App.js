cf6db5fd6813bcfaa03686b2985dfda06a83a445
khaled hannachi <hannachikhaled22@gmail.com>
2023-12-21 03:38:08 -0800

    react

---
+++ MERNStack/week_1/Day_3/Core/first_react_with_jsx/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          Hello Dojo!
+        </h1>
+        <h2>
+          Things I need to do:
+        </h2>
+        <div>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
